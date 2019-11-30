29056b7a7db1af05a0c30e0732a27d7711259a7c
Maximilian Peters <maximilian.peters1@rwth-aachen.de>
2019-11-30 13:47:55 +0100

    added type and score

--- scripts/elasticsearch.py
+++ scripts/elasticsearch.py
@@ -2,17 +2,22 @@ import requests
 
 def search(domain, query):
     url = 'http://%s/freebase/label/_search' % domain
-    response = requests.get(url, params={'q': query, 'size':1000})
+    response = requests.get(url, params={'q': query, 'size': 100})
     id_labels = {}
     if response:
         response = response.json()
         for hit in response.get('hits', {}).get('hits', []):
             freebase_label = hit.get('_source', {}).get('label')
             freebase_id = hit.get('_source', {}).get('resource')
-            id_labels.setdefault(freebase_id, set()).add( freebase_label )
-    return id_labels
+            freebase_score = hit.get('_score')
+			freebase_type = hit.get('type')
+			
+            freebase_number_of_facts = 0
+            freebase_match = 0
+            freebase_similarity = 0
+            id_labels.setdefault(freebase_id, set()).add( freebase_label ).add( freebase_type).add(freebase_score).add(freebase_number_of_facts).add(freebase_match).add(freebase_similarity)
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     import sys
     try:
         _, DOMAIN, QUERY = sys.argv
