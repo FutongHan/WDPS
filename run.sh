ed3997b4198b40e37c4af71fde876cac7cc49252
RJ <rj.evangelista@hotmail.com>
2019-12-04 11:45:49 +0100

    add start trident server

--- run.sh
+++ run.sh
@@ -16,10 +16,23 @@ echo "elasticsearch should be running now on node $ES_NODE:$ES_PORT (connected t
 
 # TODO: Start Trident server
 ############################
+KB_PORT=9090
+KB_BIN=/home/jurbani/trident/build/trident
+KB_PATH=/home/jurbani/data/motherkb-trident
 
-# Entity recognition
+echo "Lauching an instance of the Trident server on a random node in the cluster ..."
+prun -o .kb_log -v -np 1 $KB_BIN server -i $KB_PATH --port $KB_PORT </dev/null 2> .kb_node &
+echo "Waiting 5 seconds for trident to set up (use 'preserve -llist' to see if the node has been allocated)"
+until [ -n "$KB_NODE" ]; do KB_NODE=$(cat .kb_node | grep '^:' | grep -oP '(node...)'); done
+sleep 5
+KB_PID=$!
+echo "Trident should be running now on node $KB_NODE:$KB_PORT (connected to process $KB_PID)"
+
+# Entity recognition and linking
 python run.py $ES_NODE:$ES_PORT
 
+# Stop Trident server
+kill $KB_PID
 
 # Stop Elasticsearch server
 kill $ES_PID
