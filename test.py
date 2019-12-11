e9f31a9d28772c13096d978e67f14ae6eb4b6f85
RJ <rj.evangelista@hotmail.com>
2019-12-11 18:42:43 +0100

    test

--- test.py
+++ test.py
@@ -60,39 +60,40 @@ def record2html(record):
 
 
 def html2text(record):
+    _,record = record 
     html_doc = record2html(record)
-    # # Rule = "/<.*>/";
-    # useless_tags = ['footer', 'header', 'sidebar', 'sidebar-right',
-    #                 'sidebar-left', 'sidebar-wrapper', 'wrapwidget', 'widget']
-    # if html_doc:
-    #     soup = BeautifulSoup(html_doc, "html.parser")
-    #     # remove tags: <script> <style> <code> <title> <head>
-    #     [s.extract() for s in soup(
-    #         ['script', 'style', 'code', 'title', 'head', 'footer', 'header'])]
-    #     # remove tags id= useless_tags
-    #     [s.extract() for s in soup.find_all(id=useless_tags)]
-    #     # remove tags class = useless_tags
-    #     [s.extract() for s in soup.find_all(
-    #         name='div', attrs={"class": useless_tags})]
-    #     # remove comments
-    #     for element in soup(s=lambda s: isinstance(s, Comment)):
-    #         element.extract()
-    #     # text = soup.get_text("\n", strip=True)
+    # Rule = "/<.*>/";
+    useless_tags = ['footer', 'header', 'sidebar', 'sidebar-right',
+                    'sidebar-left', 'sidebar-wrapper', 'wrapwidget', 'widget']
+    if html_doc:
+        soup = BeautifulSoup(html_doc, "html.parser")
+        # remove tags: <script> <style> <code> <title> <head>
+        [s.extract() for s in soup(
+            ['script', 'style', 'code', 'title', 'head', 'footer', 'header'])]
+        # remove tags id= useless_tags
+        [s.extract() for s in soup.find_all(id=useless_tags)]
+        # remove tags class = useless_tags
+        [s.extract() for s in soup.find_all(
+            name='div', attrs={"class": useless_tags})]
+        # remove comments
+        for element in soup(s=lambda s: isinstance(s, Comment)):
+            element.extract()
+        # text = soup.get_text("\n", strip=True)
 
-    #     # get text in <p></p>
-    #     paragraph = soup.find_all("p")
-    #     text = ""
-    #     for p in paragraph:
-    #         if p.get_text(" ", strip=True) != '':
-    #             text += p.get_text(" ", strip=True)+"\n"
-    #     if text == "":
-    #         text = soup.get_text(" ", strip=True)
-    #     # text = re.sub(Rule, "", text)
-    #     # escape character
-    #     # soup_sec = BeautifulSoup(text,"html.parser")
+        # get text in <p></p>
+        paragraph = soup.find_all("p")
+        text = ""
+        for p in paragraph:
+            if p.get_text(" ", strip=True) != '':
+                text += p.get_text(" ", strip=True)+"\n"
+        if text == "":
+            text = soup.get_text(" ", strip=True)
+        # text = re.sub(Rule, "", text)
+        # escape character
+        # soup_sec = BeautifulSoup(text,"html.parser")
 
 
-        # yield text
+        yield text
     yield "yo"
 
 
