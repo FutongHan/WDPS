48cb04ba1b70572b53095cfa9ebbfda3d4a926f4
Maximilian Peters <maximilian.peters1@rwth-aachen.de>
2019-12-12 18:47:01 +0100

    te

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 # Activate the virtual env
 source .env/bin/activate
 module load prun
+module load hadoop
 export SPARK_HOME=/home/bbkruit/spark-2.4.0-bin-without-hadoop
 
 # Start Elasticsearch server
