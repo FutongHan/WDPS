061feec27d3161c2d35155a58ffe9c93e6901f08
Maximilian Peters <maximilian.peters1@rwth-aachen.de>
2019-12-12 19:10:59 +0100

    te

--- test.sh
+++ test.sh
@@ -39,7 +39,9 @@ echo "Trident should be running now on node $KB_NODE:$KB_PORT (connected to proc
 hdfs dfs -rm -r /user/wdps1911/sample
 
 prun -np 1 -t $TIME /home/bbkruit/spark-2.4.0-bin-without-hadoop/bin/spark-submit \
---master local[4] \
+#--master local[4] \
+--master yarn \
+--deploy-mode cluster \
 --driver-memory 8G \
 --executor-memory 8G \
 --num-executors 8 test.py $ES_NODE:$ES_PORT $KB_NODE:$KB_PORT
