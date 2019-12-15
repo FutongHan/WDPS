bdbfd22199ad35c5f8d45f6f4ef982d0ac76502b
Maximilian Peters <maximilian.peters1@rwth-aachen.de>
2019-12-15 15:32:24 +0100

    t

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 # Set up
 source .env/bin/activate
-virtualenv --relocatable .env
 module load prun
-module load hadoop
 export SPARK_HOME=/home/wdps1911/spark
 export SPARK_LOCAL_DIRS=/home/wdps1911/tmp
 export PYSPARK_PYTHON=/home/wdps1911/WDPS2019/.env/bin/python3
 export YARN_CONF_DIR=/cm/shared/package/hadoop/hadoop-2.7.6/etc/hadoop
 
-TIME=30:00
+TIME=45:00
 
 INPUT=${1:-"hdfs:///user/bbkruit/sample.warc.gz"}
 OUTPUT=${2:-"output"}
@@ -47,11 +45,10 @@ echo "Trident should be running now on node $KB_NODE:$KB_PORT (connected to proc
 ############################
 
 prun -np 1 -t $TIME $SPARK_HOME/bin/spark-submit \
-    --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./ENV/bin/python3 \
-    --master local[16] \
-    --executor-memory 4G \
-    --num-executors 16 \
-    --archives venv.zip#ENV \
+    --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=$PYSPARK_PYTHON \
+    --master local[8] \
+    --executor-memory 8G \
+    --num-executors 8 \
     run.py $ES_NODE:$ES_PORT $KB_NODE:$KB_PORT $INPUT $OUTPUT
 
 hdfs dfs -copyToLocal $OUTPUT $OUTPUT
