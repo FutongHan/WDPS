5f46c19e393a5977775be85200b11d17293a71eb
Maximilian Peters <maximilian.peters1@rwth-aachen.de>
2019-12-15 22:31:39 +0100

    g

--- run.py
+++ run.py
@@ -61,7 +61,7 @@ def html_to_text(record):
 ##### ENTITY RECOGNITION #####
 def named_entity_recognition(record):
     key, html = record
-    SPACY = spacy.load("en_core_web_lg")
+
     doc = SPACY(html)
 
     for mention in doc.ents:
@@ -163,7 +163,7 @@ if __name__ == "__main__":
     except Exception:
         print('Usage: DOMAIN_ES, DOMAIN_TRIDENT')
         sys.exit(0)
-
+    SPACY = spacy.load("en_core_web_sm")
     # Spark setup with conf from command line
     sc = SparkContext()
     # split WARC
