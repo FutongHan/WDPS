d2e8442ac82ff0a22d2bb7252084a57c4118debc
Maximilian Peters <maximilian.peters1@rwth-aachen.de>
2019-12-18 09:20:09 +0100

    g

--- run.py
+++ run.py
@@ -167,7 +167,9 @@ if __name__ == "__main__":
         sys.exit(0)
     SPACY = spacy.load("en_core_web_sm")
     # Spark setup with conf from command line
-    sc = SparkContext()
+    # sc = SparkContext()
+    
+    sc = SparkContext("yarn", "wdps1911")
     # split WARC
     config = {"textinputformat.record.delimiter": "WARC/1.0"}
 
